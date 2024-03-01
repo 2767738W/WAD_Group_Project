3f4e16bfa7e9ea27b85bca76eff6ecb3bcc5e08e
“Will <willwalker@glaroam2-164-105.wireless.gla.ac.uk>
2024-03-01 13:57:41 +0000

    urls

--- wad/urls.py
+++ wad/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
+    path('cuisine/', views.cuisine, name='cuisine'),
+    path('cuisine/<slug:cuisine_name_slug>/', views.cuisine_detail, name='cuisine_detail'),
+    path('cuisine/<slug:cuisine_name_slug>/addrecipe/', views.add_recipe, name='add_recipe'),
+    path('cuisine/<slug:cuisine_name_slug>/viewrecipe/', views.view_recipes, name='view_recipes'),
 ]
