01b2cee3ddc031df82001d9c6aff01f75e957500
“Will <willwalker@Wills-MacBook-Air.local>
2024-03-18 16:13:38 +0000

    Ingredients filter for all cuisines

--- wad/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from wad.models import UserProfile, Recipe, starRating
-
-admin.site.register(UserProfile)
-admin.site.register(Recipe)
-admin.site.register(starRating)
-
--- wad/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WadConfig(AppConfig):
-    name = 'wad'
