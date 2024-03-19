f1e494d11d20afd0e5f56c450099784b7f48b08a
Lewis Murdoch <2773179m@student.gla.ac.uk>
2024-03-19 11:33:31 +0000

    code

--- wad/forms.py
+++ wad/forms.py
@@ -7,16 +7,7 @@ from django.core.exceptions import ValidationError
 
 #need form for recipes to collect information submitted by users adding recipes
 #also need form for users leaving ratings, should link to the starRating model as that is where ratings are stored
-# class RecipeForm(forms.ModelForm):
-#     name = forms.CharField(max_length = Recipe.NAME_MAX_LENGTH, help_text = "Please enter the Recipe name. ")
-#     cuisine = forms.ChoiceField(choices = Recipe.CUISINE_CHOICES, help_text = "Please select a cuisine. ")
-#     ingredients = forms.CharField(widget = forms.Textarea, help_text = "Please enter the ingredients. ")
-#     instructions = forms.CharField(widget = forms.Textarea, help_text = "Please enter the instructions. ")
-#     image = forms.ImageField(help_text = "Please upload a picture of your recipe. ")
 
-#     class Meta:
-#         model = Recipe
-#         fields = ('name', 'cuisine', 'ingredients', 'instructions', 'image',)
         
 class RecipeForm(forms.ModelForm):
     name = forms.CharField(
